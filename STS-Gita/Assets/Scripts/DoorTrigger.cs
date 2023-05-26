9c55ca9f45a052fa080cb83d730f87edb83d5d51
Widyarso Joko Purnomo <widyjp10@gmail.com>
2023-05-26 15:21:42 +0700

    add door trigger

---
+++ STS-Gita/Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject playerDoorIndicator;
+    public SceneFader sceneFader;
+    private string sceneToLoad = "Chapter 1 - Classroom";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            playerDoorIndicator.SetActive(true);
+            Debug.Log("Door Detected");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerDoorIndicator.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if(playerDoorIndicator.activeInHierarchy && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            sceneFader.FadeTo(sceneToLoad);
+        }
+    }
+}
