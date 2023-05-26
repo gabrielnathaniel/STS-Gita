74f61b58bea891571a05287c132bdfbe0968ef5e
gabrielnathaniel <gabrielnathaniel1@gmail.com>
2023-05-26 16:50:54 +0700

    add health bar and candy land background music

--- STS-Gita/Assets/Scripts/PlayerHealth.cs
+++ STS-Gita/Assets/Scripts/PlayerHealth.cs
@@ -7,15 +7,25 @@ public class PlayerHealth : MonoBehaviour
 {
 	public Animator animator;
 
-	public int health = 100;
+	public int maxHealth = 100;
+	public int currentHealth;
 
 	public GameObject deathEffect;
 
+	public HealthBar healthBar;
+
+	void Start()
+	{
+		currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+	}
+
 	public void TakeDamage(int damage)
 	{
-		health -= damage;
+		currentHealth -= damage;
 
-		// StartCoroutine(DamageAnimation());
+		healthBar.SetHealth(currentHealth);
+
 		DamageAnimation();
 
 		// if (health <= 0)
@@ -29,34 +39,12 @@ public class PlayerHealth : MonoBehaviour
 	// 	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	// }
 
-	// IEnumerator DamageAnimation()
 	void DamageAnimation()
 	{
 		SpriteRenderer[] srs = GetComponentsInChildren<SpriteRenderer>();
 
 		// Play hurt animation
         animator.SetTrigger("Hurt");
-
-		// for (int i = 0; i < 3; i++)
-		// {
-		// 	foreach (SpriteRenderer sr in srs)
-		// 	{
-		// 		Color c = sr.color;
-		// 		c.a = 0;
-		// 		sr.color = c;
-		// 	}
-
-		// 	yield return new WaitForSeconds(.1f);
-
-		// 	foreach (SpriteRenderer sr in srs)
-		// 	{
-		// 		Color c = sr.color;
-		// 		c.a = 1;
-		// 		sr.color = c;
-		// 	}
-
-		// 	yield return new WaitForSeconds(.1f);
-		// }
 	}
 
 }
