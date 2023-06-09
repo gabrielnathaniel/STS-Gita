a251c61d88cda2cc2ffdbcb725401b424c9481ed
gabrielnathaniel <gabrielnathaniel1@gmail.com>
2023-06-09 17:01:46 +0700

    finishing chapter 2 story

---
+++ STS-Gita/Assets/Scripts/Deactivate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deactivate : MonoBehaviour
+{
+    public void Deactive()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
